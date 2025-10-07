f8a8b77d2f49dad7106ba118b87fea35cf31a1e2
halfstorm <halfstorm@nexigate.win>
2025-10-07 12:11:19 -0500

    Removed old Vivecraft beta local jar Disabled debug Updated version to full 1.2.0

--- src/main/java/win/demistorm/VRThrowingExtensions.java
+++ src/main/java/win/demistorm/VRThrowingExtensions.java
@@ -23,7 +23,7 @@ public class VRThrowingExtensions implements ModInitializer {
 	public static EntityType<ThrownProjectileEntity> THROWN_ITEM_TYPE;
 
 	// DEBUG mode on/off
-	public static final boolean debugMode = true;
+	public static final boolean debugMode = false;
 
 	static {
 		Configurator.setLevel(MOD_ID, debugMode ? Level.DEBUG : Level.INFO);
